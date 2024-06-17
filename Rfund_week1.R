cd1602cd65ddf5d90652a8115205596ce7775a9f
Gabby Panayotakis <gmp175@scarletmail.rutgers.edu>
2024-06-17 10:39:55 -0400

    finished with week 1 videos

--- Rfund_week1.R
+++ Rfund_week1.R
@@ -93,7 +93,52 @@ z[["bar"]]
 #single bracket with the name gives a list with the element bar in it (single bracket will return a list if subsetting a list)
 #using the name means you don't have to remember where in the list it is
 
-#single bracket to extract multiple elements
+
 a <- list(boo = 1:4, far = 0.9, baz = "hello")
+#single bracket to extract multiple elements (can't use double bracket or dollar sign)
 a[c(1, 3)]
 #double bracket to index a list where the index itself is computed
+#dollar sign will look for the element that has name in it
+name <- "boo"
+a[[name]]
+a$name
+a$boo
+
+#subsetting nested elements of a list
+b <- list(a = list(10, 12, 14), d = c(3.14, 2.81))
+b[[c(1,3)]]
+b[[1]][[3]]
+b[[c(2, 1)]]
+
+#matrix
+#first index is row, second index is column
+c <- matrix(1:6, 2, 3)
+c[1, 2] #returns a numerical value
+c[2, 1]
+#indices missing
+c[1, ] #first row of matrix, won't get a matrix back, get back a vector instead
+c[, 2] #second column
+c[1, 2, drop = FALSE] #returns a matrix, preserving the dimensions of the object
+c[1, , drop = FALSE] #returns a matrix
+
+#partial matching 
+d <- list(apple = 1:5)
+d$a #searches the list and pulls the value starting with an a
+d[["a"]] #expects an exact match to the name
+d[["a", exact = FALSE]] #exact argument is overridden
+
+#removing NA values
+e <- c(1,2, NA, 4, NA, 5) #simple vector with missing values
+bad <- is.na(e) #is.na function tells you which values are NA
+e[!bad] #bad is a logical vector - true if missing, false if not missing #! takes the opposite of bad
+
+f <- c("a", "b", NA, "d", NA, "f")
+good <- complete.cases(e, f) #complete.cases tell which positions have both elements nonmissing
+good #when you subset you only get the good elements
+e[good]
+f[good]
+
+#logical vector tells which areas are complete
+airquality[1:6, ]
+good <- complete.cases(airquality) #subset out the matrix for only good, subsetting out missing values
+airquality[good, ][1:6, ]
